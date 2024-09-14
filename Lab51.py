e642c96dbdfc94b684c5a3dd9f219952d3773665
Alexandr_Kisliy <sashakisliy2006@gmail.com>
2024-09-14 18:21:54 +0300

    git commit

--- Lab51.py
+++ Lab51.py
@@ -1,9 +1,13 @@
 n = int(input("n = "))
 sum=0
+sum_of_minus=0
 print(f"Enter {n} array elements:")
 arr = [int(input()) for _ in range(n)]
 print("Original array: ", arr)
 for num in arr:
-    if num % 3 == 0:
+    if num<0:
+        sum_of_minus+=num
+    else:
         sum += num
 print(f"Sum: {sum}")
+print(f"Sum of minus: {sum_of_minus}")
